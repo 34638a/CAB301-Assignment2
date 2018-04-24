4e2923cdb3683359f72b246c7baf5841298b8856
Jordan R <jordanr123@bigpond.com>
2018-04-24 17:15:00 +1000

    Add Common Files

---
+++ src/assignments/assignment2/core/Main.java
@@ -0,0 +1,7 @@
+package assignments.assignment2.core;
+
+/**
+ * Created by Jordan Laptop on 24/04/2018.
+ */
+public class Main {
+}
---
+++ src/assignments/assignment2/jack/Median.java
@@ -0,0 +1,7 @@
+package assignments.assignment2.jack;
+
+/**
+ * Created by Jordan Laptop on 24/04/2018.
+ */
+public class Median {
+}

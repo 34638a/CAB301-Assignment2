fe770d67c6b23f48dbb88bf9a2328945a171d3cf
Jordan R <jordanr123@bigpond.com>
2018-04-30 17:25:38 +1000

    Removed Comments from BruteForceMedian.java

--- src/assignments/assignment2/jordan/BruteForceMedian.java
+++ src/assignments/assignment2/jordan/BruteForceMedian.java
@@ -1,31 +1,9 @@
 package assignments.assignment2.jordan;
 
-import java.util.Arrays;
-
 /**
  * Created by Jordan Laptop on 24/04/2018.
  */
 public class BruteForceMedian {
-
-	/*
-	ALGORITHM BruteForceMedian(A[0..n − 1])
- // Returns the median value in a given array A of n numbers. This is
- // the kth element, where k = n 2, if the array was sorted.
- k ← n 2
- for i in 0 to n − 1 do
- numsmaller ← 0 // How many elements are smaller than A[i]
- numequal ← 0 // How many elements are equal to A[i]
- for j in 0 to n − 1 do
- if A[j] < A[i] then
- numsmaller ← numsmaller + 1
- else
- if A[j] = A[i] then
- numequal ← numequal + 1
- if numsmaller < k and k ≤ (numsmaller + numequal) then
- return A[i]
-	 */
-
-
 
 	/**
 	 * Given any array of length n with the data type of primitive integer(32 bits long). Find the median of the array.
